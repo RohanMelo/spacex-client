801e0dd2094303af6e5f8d92f810c1ab44134eb3
João Gomes <jlfgomes@gmail.com>
2018-10-19 19:18:07 +0200

    Bug de não renderizar heroes no Main consertado (#3)

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,29 +2,32 @@ import React, { Component } from "react";
 
 import MainHero from './MainHero';
 import axios from 'axios';
-// import launchData from '../mockdata/LAUNCH.json'
+import launchData from '../mockdata/LAUNCH.json'
 
 class Main extends Component {
   state = {
-    apiLaunch: []
+    apiLaunch: {},
+    loading: true,
   };
   
   componentDidMount() {
     axios.get('https://api.spacexdata.com/v3/launches/latest')
       .then(res => {
         this.setState({
-          apiLaunch: res.data
-        }).catch((e) => {
-          console.error(e);
+          apiLaunch: res.data,
+          loading: false
+        }, error => {
+          console.log(error)
         });
       })
   };
   
   render() {
-    return (
+    if (this.state.loading) return <h1>loading...</h1>
+    else return (
       <section>
         <MainHero launch={this.state.apiLaunch} />
-        <MainHero launch={this.state.apiLaunch} latest />
+        <MainHero launch={launchData} latest />
       </section>
     );
   };
